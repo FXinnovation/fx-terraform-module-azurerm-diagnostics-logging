267950b52c1948c643936fb56a0a5b3f4150b959
Maxime Roy <maxime.roy@fxinnovation.com>
2019-11-26 12:09:27 -0500

    Added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,24 @@
 output "diagnostics_map" {
   value = map(
-    "diags_sa", "${azurerm_storage_account.log.id}",
-    "eh_name", "${azurerm_eventhub_namespace.log.name}",
-    "eh_id", "${azurerm_eventhub_namespace.log.id}"
+    "diags_sa", azurerm_storage_account.this.id,
+    "eh_name", azurerm_eventhub_namespace.this.name,
+    "eh_id", azurerm_eventhub_namespace.this.id,
+    "la_name", var.log_analytics_workspace_name,
+    "la_id", module.log-analytics-workspace.id,
+    "la_work_id", module.log-analytics-workspace.workspace_id
   )
+}
+
+output "log_analytics_primary_shared_key" {
+  sensitive = true
+  value     = module.log-analytics-workspace.primary_shared_key
+}
+
+output "log_analytics_secondary_shared_key" {
+  sensitive = true
+  value     = module.log-analytics-workspace.secondary_shared_key
+}
+
+output "log_analytics_workspace_id" {
+  value = module.log-analytics-workspace.workspace_id
 }
